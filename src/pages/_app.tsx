4d4e28fa2b38ad650af644d44e443074a8a4be8e
ilya <pasterzoom@gmail.com>
2023-04-26 11:37:01 +0300

    init: install libs, setting up basic configurations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,50 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode, useState } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { MantineProvider } from '@mantine/core'
+import { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const App = ({ Component, pageProps }: AppPropsWithLayout): ReactNode => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { refetchOnWindowFocus: false } },
+      })
+  )
+  const getLayout = Component.getLayout ?? (page => page)
+
+  return getLayout(
+    <>
+      <Head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+        }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+        </QueryClientProvider>
+      </MantineProvider>
+    </>
+  )
+}
+
+export default App
