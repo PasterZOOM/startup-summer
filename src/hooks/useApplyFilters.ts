f101a0bddbc348f0914d8d03c9279fb508c7faa0
ilya <pasterzoom@gmail.com>
2023-05-01 21:24:56 +0300

    fix: default page value

--- src/hooks/useApplyFilters.ts
+++ src/hooks/useApplyFilters.ts
@@ -4,13 +4,12 @@ import { useRouter } from 'next/router'
 
 import { ParamsKey } from '@/api/vacancies/types'
 import { useGetAllVacancies } from '@/hooks/query/useGetAllVacancies'
-import { selectPage, selectParamsState, useParamsStore } from '@/store/useParamsStore'
+import { selectParamsState, useParamsStore } from '@/store/useParamsStore'
 
 export const useApplyFilters = (): (() => void) => {
   const { refetch } = useGetAllVacancies()
   const { pathname, replace, query } = useRouter()
   const [params] = useParamsStore(selectParamsState)
-  const [page, setPage] = useParamsStore(selectPage)
 
   const applyFilters = async (): Promise<void> => {
     const queryParams: Partial<Record<ParamsKey, string | []>> = {}
@@ -19,15 +18,9 @@ export const useApplyFilters = (): (() => void) => {
       queryParams[key as keyof typeof queryParams] = value || []
     })
 
-    await replace({ pathname, query: queryParams }, undefined, {
+    await replace({ pathname, query: { ...queryParams, page: '0' } }, undefined, {
       shallow: true,
     })
-
-    if (page === '0') {
-      await refetch()
-    } else {
-      setPage('0')
-    }
   }
 
   useEffect(() => {
