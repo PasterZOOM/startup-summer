7f57630a9147cd9eb77bb51db2c4aa46ce13938b
ilya <pasterzoom@gmail.com>
2023-05-13 10:51:10 +0300

    fix: FavoriteStare checkbox

--- src/components/common/ui/checkboxes/favoriteStare.tsx
+++ src/components/common/ui/checkboxes/favoriteStare.tsx
@@ -18,17 +18,24 @@ export const FavoriteStare: FC<PropsType> = ({ vacancy }) => {
   }
 
   return (
-    <div onClick={onFavoriteStareClick} onKeyDown={onKeyEnter} aria-hidden>
+    <div
+      onClick={onFavoriteStareClick}
+      onKeyDown={onKeyEnter}
+      aria-hidden
+      className="flex cursor-pointer items-center justify-center"
+    >
       <input
         data-elem={`vacancy-${vacancy.id}-shortlist-button`}
         type="checkbox"
+        value={inFavorite.toString()}
         checked={inFavorite}
         onChange={onFavoriteStareClick}
-        hidden
+        className="absolute opacity-0"
+        tabIndex={-1}
       />
       <StarIcon
         tabIndex={0}
-        className={`cursor-pointer transition hover:fill-blue-400 hover:text-blue-400 focus:outline-0 focus:hover:fill-blue-400 ${
+        className={`relative transition hover:fill-blue-400 hover:text-blue-400 focus:outline-0 focus:hover:fill-blue-400 ${
           inFavorite
             ? 'fill-blue-main-500 text-blue-main-500 focus:fill-blue-600 focus:text-blue-600'
             : 'text-gray-500 focus:text-blue-main-500'
